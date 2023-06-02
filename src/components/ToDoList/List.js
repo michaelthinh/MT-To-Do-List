2f3402fbde27d29889ae00ed7ab0da9baa1bee16
Michael Thinh <michaelthinh1008@gmail.com>
2023-06-02 12:15:08 +0700

    add filter function

--- src/components/ToDoList/List.js
+++ src/components/ToDoList/List.js
@@ -2,6 +2,7 @@ import ListUI from "../UI/ListUI";
 import ListInfo from "./ListInfo/ListInfo";
 import ListContent from "./ListContent/ListContent";
 import ListActions from "./ListActions/ListActions";
+import { useState } from "react";
 
 const DUMMY_LIST = [
     {
@@ -17,10 +18,20 @@ const DUMMY_LIST = [
 ];
 
 const List = (props) => {
+    const [list, setList] = useState(DUMMY_LIST);
+    const getFilterHandler = (filter) => {
+        if (filter === "active") {
+            setList(DUMMY_LIST.filter((item) => item.status === "Active"));
+        } else if (filter === "completed") {
+            setList(DUMMY_LIST.filter((item) => item.status === "Completed"));
+        } else {
+            setList(DUMMY_LIST);
+        }
+    };
     return (
         <ListUI>
-            <ListInfo />
-            <ListContent list={DUMMY_LIST} />
+            <ListInfo onGetFilter={getFilterHandler} />
+            <ListContent list={list} />
             <ListActions />
         </ListUI>
     );
--- src/components/ToDoList/ListInfo/ListInfo.js
+++ src/components/ToDoList/ListInfo/ListInfo.js
@@ -5,6 +5,10 @@ const ListInfo = (props) => {
     const date = getDate();
     const { time } = useGetTime();
     const classActive = "text-[#ff1ed2]";
+    const filterHandler = (e) => {
+        const filter = e.target.id;
+        props.onGetFilter(filter);
+    };
     return (
         <div className="border-b-2 border-[#7a4b4b] p-4 ">
             <div className="flex flex-wrap justify-between content-center">
@@ -13,9 +17,15 @@ const ListInfo = (props) => {
                     <div>{date}</div>
                 </div>
                 <div className="flex flex-wrap gap-5 content-center">
-                    <button className={classActive}>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
+                    <button id="all" onClick={filterHandler}>
+                        All
+                    </button>
+                    <button id="active" onClick={filterHandler}>
+                        Active
+                    </button>
+                    <button id="completed" onClick={filterHandler}>
+                        Completed
+                    </button>
                 </div>
             </div>
         </div>
